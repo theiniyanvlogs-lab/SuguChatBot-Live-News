18b65956746cf05ede80a9f379fb3a0beea53280
theiniyanvlogs-lab <the.iniyan.vlogs@gmail.com>
2026-07-25 19:09:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,11 +4,4 @@ const CONFIG = {
     API_KEY: "pub_2196872b24c34581be0b69a45709f2dd",
     BASE_URL: "https://newsdata.io/api/1/latest",
 
-    // Groq
-    GROQ_API_KEY: "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",
-
-    GROQ_URL: "https://api.groq.com/openai/v1/chat/completions",
-
-    GROQ_MODEL: "llama-3.3-70b-versatile"
-
 };
